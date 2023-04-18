35e02b964fc602bff713972b103c1b1f601c3c42
Abenezer Tesfaye <abenezertesfaye92@gmail.com>
2023-04-18 14:15:03 +0300

    responsive Ecommerce Landing page using html and css

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const links = document.querySelectorAll('li');
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+
+if(bar){
+    bar.addEventListener("click", () => {
+      nav.classList.add("show");
+    });
+}
+
+close.addEventListener("click", () => {
+    nav.classList.remove("show");
+})
+
